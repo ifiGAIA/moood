9187c5b08c8d7c0d7c1085b96ce1be62af0645a5
ifigaia <wet159@gmail.com>
2020-05-29 16:21:46 +0800

    test

--- App.js
+++ App.js
@@ -148,7 +148,7 @@ const App = () => {
         }
       >
         <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login}
+          <Stack.Screen name="Loogin" component={Login}
             options={{
               headerShown: false
             }} />
